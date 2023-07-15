a6414c13b0ddacedf0b6d83c33a8188c87c24826
Yisustxz <jesusoortiz2410@gmail.com>
2023-07-15 01:38:21 -0400

    listo el add y get de trabajador

--- src/TablePages/TableRows/TrabajadoresRows.jsx
+++ src/TablePages/TableRows/TrabajadoresRows.jsx
@@ -1,29 +1,172 @@
-import { FaEdit, FaTrash } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { FaEdit, FaTrash } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
-export default function TrabajadoresRows() {
-
-    return (
-      <div>
-          <div style={{ width: '64vw', height: 'auto', marginTop: '1vh', borderBottomColor: '#C1BFBF', borderWidth: '2px', borderTop: 'none', borderLeft: 'none', borderRight: 'none', display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
-            <div style={{ width: '57vw', minHeight: 'auto', display: 'flex', alignItems: 'center', flexDirection: 'row', fontSize: '16px', borderRightColor: '#C1BFBF', borderLeft: 'none', borderTop: 'none', borderBottom: 'none', borderWidth: '2px'}}>
-                {/* 
+export default function TrabajadoresRows({
+  ci_trabajador,
+  nombre_trabajador,
+  direccion_trabajador,
+  telefono_trabajador,
+  sueldo_trabajador,
+  cargo
+}) {
+  return (
+    <div>
+      <div
+        style={{
+          width: '64vw',
+          height: 'auto',
+          marginTop: '1vh',
+          borderBottomColor: '#C1BFBF',
+          borderWidth: '2px',
+          borderTop: 'none',
+          borderLeft: 'none',
+          borderRight: 'none',
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'row'
+        }}
+      >
+        <div
+          style={{
+            width: '57vw',
+            minHeight: 'auto',
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: 'row',
+            fontSize: '16px',
+            borderRightColor: '#C1BFBF',
+            borderLeft: 'none',
+            borderTop: 'none',
+            borderBottom: 'none',
+            borderWidth: '2px'
+          }}
+        >
+          {/* 
                 Colocar las variables donde está el texto :)
                  */}
-                <div style={{ width: '11vw', minHeight: '4vh', display: 'flex', flexDirection: 'row', justifyContent: 'center', flex: 1, alignItems: 'center', borderRightColor: '#D7D5D5', borderLeft: 'none', borderTop: 'none', borderBottom: 'none', borderWidth: '2px'}}>28555555</div>
-                <div style={{ width: '11vw', minHeight: '4vh', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', flex: 1,  overflow: 'auto', borderRightColor: '#D7D5D5', borderLeft: 'none', borderTop: 'none', borderBottom: 'none', borderWidth: '2px'}}>Ana García Salazar</div>
-                <div style={{ width: '11vw', minHeight: '4vh', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', flex: 1, overflow: 'auto', borderRightColor: '#D7D5D5', borderLeft: 'none', borderTop: 'none', borderBottom: 'none', borderWidth: '2px' }}>Los Olivos</div>
-                <div style={{ width: '11vw', minHeight: '4vh', display: 'flex', flexDirection: 'row', justifyContent: 'center', flex: 1, alignItems: 'center', borderRightColor: '#D7D5D5', borderLeft: 'none', borderTop: 'none', borderBottom: 'none', borderWidth: '2px'}}>04246666666</div>
-                <div style={{ width: '11vw', minHeight: '4vh', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', flex: 1,  overflow: 'auto', borderRightColor: '#D7D5D5', borderLeft: 'none', borderTop: 'none', borderBottom: 'none', borderWidth: '2px'}}>200</div>
-                <div style={{ width: '11vw', minHeight: '4vh', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', flex: 1 }}>Analista</div>
-            </div>
-            <div style={{ minHeight: '4vh', width: '7vw', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around' }}>
-            <Link to="/EditTrabajador">
-            <FaEdit color={"#192C45"} size={25} style={{ cursor: 'pointer' }} />
-             </Link>
-            <FaTrash color={"#192C45"} size={25} style={{cursor: 'pointer'}}/>
-            </div>
-          </div> 
+          <div
+            style={{
+              width: '11vw',
+              minHeight: '4vh',
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              flex: 1,
+              alignItems: 'center',
+              borderRightColor: '#D7D5D5',
+              borderLeft: 'none',
+              borderTop: 'none',
+              borderBottom: 'none',
+              borderWidth: '2px'
+            }}
+          >
+            {ci_trabajador}
+          </div>
+          <div
+            style={{
+              width: '11vw',
+              minHeight: '4vh',
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flex: 1,
+              overflow: 'auto',
+              borderRightColor: '#D7D5D5',
+              borderLeft: 'none',
+              borderTop: 'none',
+              borderBottom: 'none',
+              borderWidth: '2px'
+            }}
+          >
+            {nombre_trabajador}
+          </div>
+          <div
+            style={{
+              width: '11vw',
+              minHeight: '4vh',
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flex: 1,
+              overflow: 'auto',
+              borderRightColor: '#D7D5D5',
+              borderLeft: 'none',
+              borderTop: 'none',
+              borderBottom: 'none',
+              borderWidth: '2px'
+            }}
+          >
+            {direccion_trabajador}
+          </div>
+          <div
+            style={{
+              width: '11vw',
+              minHeight: '4vh',
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              flex: 1,
+              alignItems: 'center',
+              borderRightColor: '#D7D5D5',
+              borderLeft: 'none',
+              borderTop: 'none',
+              borderBottom: 'none',
+              borderWidth: '2px'
+            }}
+          >
+            {telefono_trabajador}
+          </div>
+          <div
+            style={{
+              width: '11vw',
+              minHeight: '4vh',
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flex: 1,
+              overflow: 'auto',
+              borderRightColor: '#D7D5D5',
+              borderLeft: 'none',
+              borderTop: 'none',
+              borderBottom: 'none',
+              borderWidth: '2px'
+            }}
+          >
+            {sueldo_trabajador}
+          </div>
+          <div
+            style={{
+              width: '11vw',
+              minHeight: '4vh',
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flex: 1
+            }}
+          >
+            {cargo}
+          </div>
+        </div>
+        <div
+          style={{
+            minHeight: '4vh',
+            width: '7vw',
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-around'
+          }}
+        >
+          <Link to='/EditTrabajador'>
+            <FaEdit color={'#192C45'} size={25} style={{ cursor: 'pointer' }} />
+          </Link>
+          <FaTrash color={'#192C45'} size={25} style={{ cursor: 'pointer' }} />
+        </div>
       </div>
-    );
-  }
+    </div>
+  )
+}
---
+++ src/services/trabajador.services.js
@@ -0,0 +1,94 @@
+import axios from 'axios'
+import { apiUrl } from '../config'
+
+const BASE_URL = apiUrl + '/Trabajador'
+
+export const getTrabajador = async (page = 0, size = 4) => {
+  try {
+    const res = await axios.get(BASE_URL + '?size=' + size + '&page=' + page)
+    if (!res.data.items || !res.data.success) {
+      throw new Error('No se han recibido bien los datos del servidor :(')
+    }
+    return res.data
+  } catch (error) {
+    if (error.response) {
+      throw new Error(
+        error?.response?.data?.message ||
+          'Error al obtener el trabajador del backend'
+      )
+    }
+    throw error
+  }
+}
+
+export const getTrabajadorById = async (id) => {
+  try {
+    const res = await axios.get(BASE_URL + `/${id}`)
+    if (!res.data.item || !res.data.success) {
+      throw new Error('No se han recibido bien los datos del servidor :(')
+    }
+    return res.data
+  } catch (error) {
+    if (error.response) {
+      throw new Error(
+        error?.response?.data?.message || 'Error al obtener el trabajador'
+      )
+    }
+    throw error
+  }
+}
+
+export const deleteTrabajador = async (id) => {
+  try {
+    const res = await axios.delete(BASE_URL + '/' + id)
+    if (!res.data.success) {
+      throw new Error('Ha ocurrido un fallo con el backend')
+    }
+    return res.data
+  } catch (error) {
+    if (error.response) {
+      throw new Error(
+        error?.response?.data?.message || 'Error al eliminar el trabajador'
+      )
+    }
+    throw error
+  }
+}
+
+export const createTrabajador = async (trabajador) => {
+  try {
+    const res = await axios.post(BASE_URL, trabajador)
+    if (!res.data.success) {
+      console.log(res.data)
+      throw new Error('Ha ocurrido un fallo con el backend')
+    }
+
+    return res
+  } catch (error) {
+    console.log(error)
+    if (error.response) {
+      throw new Error(
+        error?.response?.data?.message || 'Error al crear el trabajador'
+      )
+    }
+    throw error
+  }
+}
+
+export const updateTrabajador = async (trabajador, id) => {
+  try {
+    const res = await axios.put(BASE_URL + '/' + id, trabajador)
+    if (!res.data.message || !res.data.success) {
+      throw new Error('Ha ocurrido un fallo con el backend')
+    }
+
+    return res
+  } catch (error) {
+    if (error.response) {
+      throw new Error(
+        error?.response?.data?.message || 'Error al editar el trabajador'
+      )
+    }
+    throw error
+  }
+}
